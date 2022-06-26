7352b563a25751a6b3c61f373cb658ea1bb3abd8
Saeed Farahi <saeed.farahi@eversports.com>
2022-06-26 19:44:28 +0200

    updated value object & entity

--- packages/shared/src/domain/value-object.ts
+++ packages/shared/src/domain/value-object.ts
@@ -4,10 +4,13 @@ export abstract class ValueObject<TProps extends { [key: string]: unknown }> {
   readonly props: Readonly<TProps>
 
   constructor(props: TProps) {
+    this.validate(props)
     this.props = Object.freeze(cloneDeep(props))
   }
 
   equals(vo?: ValueObject<TProps>): boolean {
     return vo && vo.props && isEqual(this.props, vo.props)
   }
+
+  protected abstract validate(props: TProps): void
 }

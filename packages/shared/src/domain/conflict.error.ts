d3b47e51cbbf64b22a699b98772fea7c2dab5364
Saeed Farahi <saeed.farahi@eversports.com>
2022-07-06 18:15:20 +0200

    clear

--- packages/shared/src/domain/conflict.error.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Throw this if there's a conflict in the system
- */
-export abstract class ConflictError extends Error {
-  constructor(message: string) {
-    super(message)
-  }
-}
--- packages/shared/src/domain/invalid-argument.error.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Throw this if given argument in a method/function is not valid
- */
-export abstract class InvalidArgumentError extends Error {
-  constructor(message: string) {
-    super(message)
-  }
-}
